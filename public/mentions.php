973439c706bfbf0b419bf75c9ef5f049ea2bf258
MaximeMolinillo <max_224@hotmail.fr>
2022-08-18 15:56:56 +0200

    mise en ligne 1.4

---
+++ public/mentions.php
@@ -0,0 +1,18 @@
+<?php
+
+$page = "Mentions légales";
+
+include("../templates/header.php");
+
+?>
+<div class="mentions-container">
+    <h2>Mentions légales</h2>
+    <p>Maxime Molinillo</p>
+ 
+    <p>Identité de l'hébergeur: Hostinger</p>
+</div>
+
+
+<!-- <?php
+include("../templates/footer.php");
+?> -->
--- templates/footer.php
+++ templates/footer.php
@@ -1,3 +1,13 @@
+<footer>
+    <div class="container-footer">
+    <img src="../assets/img/logoMM.svg" alt="Maxime Molinillo">
+
+        <p>Made by Maxime Molinillo  &nbsp - &nbsp 2022 &nbsp -<a href="../public/mentions.php">Mentions légales</a></p>
+        <img src="../assets/img/logoMM.svg" alt="Maxime Molinillo">
+    
+    </div>
+</footer>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/rellax/1.12.1/rellax.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.10.4/gsap.min.js"></script>
